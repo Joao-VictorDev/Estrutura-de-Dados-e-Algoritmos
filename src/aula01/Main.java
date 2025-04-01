aa74a7dbb5265a217fb7c494d3ac317e37dab8ac
Joao-VictorDev <joaovictor.dev.io@gmail.com>
2025-04-01 09:15:54 -0300

    commit

--- src/aula01/Main.java
+++ src/aula01/Main.java
@@ -2,6 +2,7 @@ package aula01;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Estrutura de Dados e Algoritmos com Java #02: Vetores e Arrays
+        Vetor vetor = new Vetor(5);
     }
 }
---
+++ src/aula01/Vetor.java
@@ -0,0 +1,10 @@
+package aula01;
+
+public class Vetor {
+    //Estrutura de Dados e Algoritmos com Java #02: Vetores e Arrays
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
