859c8796dab0e53bbdfe3311cb4b9fe401cd85af
Joao-VictorDev <joaovictor.dev.io@gmail.com>
2025-04-02 13:05:11 -0300

    commit

--- src/aula01/Main.java
+++ src/aula01/Main.java
@@ -1,12 +1,20 @@
 package aula01;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //Estrutura de Dados e Algoritmos com Java : Vetores e Arrays
 
         Vetor vetor = new Vetor(5);
 
-        vetor.addElemento("Elemento 1");
-        vetor.addElemento("Elemento 2");
+       try{
+           vetor.addElemento("Cachorro");
+           vetor.addElemento("Gato");
+           vetor.addElemento("Leão");
+           vetor.addElemento("Elefante");
+           vetor.addElemento("Rato"); // Exceção a ser lançada
+           vetor.addElemento("Cobra"); // Exceção a ser lançada
+       }catch (Exception e){
+           System.out.println("Exceção: " + e.getMessage());
+       }
     }
 }
--- src/aula01/Vetor.java
+++ src/aula01/Vetor.java
@@ -1,19 +1,22 @@
-package aula01;
+﻿package aula01;
 
 public class Vetor {
     //Estrutura de Dados e Algoritmos com Java : Vetores e Arrays
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
     }
 
-    public void addElemento(String elemento){
-        for (int i = 0; i < this.elementos.length;i++){
-            if (this.elementos[i] == null){
-                this.elementos[i] = elemento;
-                break;
-            }
+    public void addElemento(String elemento) throws Exception{
+        if (this.tamanho < this.elementos.length){
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        }else{
+            throw new Exception("Vetor Cheio");
         }
     }
+
 }
